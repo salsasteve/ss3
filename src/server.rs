d51d33e90ac6b122e4feb5c18a026fbabd164ab5
Elman Steve Laguna <steve.laguna@live.com>
2023-08-09 18:25:29 -0500

    Adding server example

--- src/server.rs
+++ src/server.rs
@@ -1,7 +1,16 @@
 use std::net::TcpListener;
+use std::io::Read;
 
 pub struct Server {
     addr: String,
+}
+
+fn arr(a: [u8; 4]) -> u32 {
+    let mut b: u32 = 0;
+    for i in 0..4 {
+        b = b + (a[i] as u32) * (256 as u32).pow(3 - i as u32);
+    }
+    b
 }
 
 impl Server {
@@ -13,6 +22,22 @@ impl Server {
         println!("Listening on {}", self.addr);
         let listener = TcpListener::bind(&self.addr).unwrap();
         
+        loop {
+            match listener.accept() {
+                Ok((mut stream, _)) => {
+                    let mut buffer = [0; 1024];
+                    match stream.read(&mut buffer) {
+                        Ok(_) => {
+                            print!("Recieved a  {}", String::from_utf8_lossy(&buffer));
+                        }
+                        Err(e) => println!("Failed to read from connection: {}", e),
+                    }
+                }
+                Err(e) => {
+                    println!("Failed to establish a connection: {}", e);
+                }
+            }
+        }
     }
 }
 
