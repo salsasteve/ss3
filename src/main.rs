482138293dd55c93add7a0cb0431489fda0d2c97
Elman Steve Laguna <steve.laguna@live.com>
2023-08-09 18:25:29 -0500

    making server for more practice

--- src/main.rs
+++ src/main.rs
@@ -1,99 +1,10 @@
-use std::{
-    error::Error,
-    io,
-    thread,
-    sync::mpsc,
-    time::{Duration, Instant}};
+use http::request::Request;
+use http::method::Method;
+use server::Server;
 
-use crossterm::{
-    cursor::{Hide, Show},
-    event::{self, Event, KeyCode},
-    terminal::{self, EnterAlternateScreen, LeaveAlternateScreen}, 
-    ExecutableCommand,
-};
-use rusty_audio::Audio;
-use ss3::{
-    frame::{self, Drawable},
-    render,
-    player::Player,
-};
-
-fn main() -> Result<(), Box<dyn Error>> {
-    let mut audio = Audio::new();
-    audio.add("explode", "assets/sounds/explode.wav");
-    audio.add("lose", "assets/sounds/lose.wav");
-    audio.add("move", "assets/sounds/move.wav");
-    audio.add("pew", "assets/sounds/pew.wav");
-    audio.add("startup", "assets/sounds/startup.wav");
-    audio.add("win", "assets/sounds/win.wav");
-    audio.play("startup");
-
-    // Terminal
-    let mut stdout: io::Stdout = io::stdout();
-    terminal::enable_raw_mode()?;
-    stdout.execute(EnterAlternateScreen)?;
-    stdout.execute(Hide)?;
-
-    // Render loop in a separate thread
-
-    let (render_tx, render_rx) = mpsc::channel();
-    let render_handle = thread::spawn(move || {
-        let mut last_frame = frame::new_frame();
-        let mut stdout = io::stdout();
-        render::render(&mut stdout, &last_frame, &last_frame, true);
-        loop {
-            let curr_frame = match render_rx.recv() {
-                Ok(x) => x,
-                Err(_) => break,
-            };
-            render::render(&mut stdout, &last_frame, &curr_frame, false);
-            last_frame = curr_frame;
-        }
-    });
-
-    // Gameloop
-    let mut player: Player = Player::new();
-    let mut instant = Instant::now();
-    'gameloop: loop {
-        let delta = instant.elapsed();
-        instant = Instant::now();
-        let mut curr_frame: Vec<Vec<&str>> = frame::new_frame();
-
-        // Input
-        while event::poll(Duration::default())? {
-            if let Event::Key(key_event) = event::read()? {
-                match key_event.code {
-                    KeyCode::Left => player.move_left(),
-                    KeyCode::Right => player.move_right(),
-                    KeyCode::Char(' ') | KeyCode::Enter => {
-                        if player.shoot() {
-                            audio.play("pew");
-                        }
-                    }
-                    KeyCode::Esc | KeyCode::Char('q') => {
-                        audio.play("lose");
-                        break 'gameloop;
-                    }
-                    _ => {}
-                }
-            }            
-        }
-
-        // Update
-        player.update(delta);
-
-        // Draw & render
-        player.draw(&mut curr_frame);
-        let _ = render_tx.send(curr_frame)?;
-        thread::sleep(Duration::from_millis(1));
-    }
-
-    // Clean up
-    drop(render_tx);
-    render_handle.join().unwrap();
-    audio.wait();
-    stdout.execute(Show)?;
-    stdout.execute(LeaveAlternateScreen)?;
-    terminal::disable_raw_mode()?;
-    Ok(())
+mod server;
+mod http;
+fn main() {
+    let server = Server::new("127.0.0.1:8080".to_string());
+    server.run();
 }
