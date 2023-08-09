c3348cc5522f6401b56d661a6bc6e2cc346851f5
Elman Steve Laguna <steve.laguna@live.com>
2023-08-09 18:25:28 -0500

    Adding assets

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+use std::error::Error;
+
+use rusty_audio::Audio;
+fn main() -> Result<(), Box<dyn Error>> {
+    let mut audio = Audio::new();
+    audio.add("explode", "assets/sounds/explode.wav");
+    audio.add("lose", "assets/sounds/lose.wav");
+    audio.add("move", "assets/sounds/move.wav");
+    audio.add("pew", "assets/sounds/pew.wav");
+    audio.add("startup", "assets/sounds/startup.wav");
+    audio.add("win", "assets/sounds/win.wav");
+    audio.play("startup");
+
+    audio.wait();
+    Ok(())
 }
