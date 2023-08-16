d2a0ed9fd0647e8534567cffd995a2f905ea1b40
SalsaSteve <steve.laguna@live.com>
2023-08-15 19:21:46 -0500

    Starting SS3

--- src/http/mod.rs
+++
@@ -1,6 +0,0 @@
-pub use method::Method;
-pub use request::Request;
-pub use request::ParseError;
-
-pub mod method;
-pub mod request;
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,26 @@
-use http::request::Request;
-use http::method::Method;
-use server::Server;
+use std::io::BufReader;
 
-mod server;
-mod http;
 fn main() {
-    let server = Server::new("127.0.0.1:8080".to_string());
-    server.run();
+    //
+    let (_stream, handle) = rodio::OutputStream::try_default().unwrap();
+    let sink = rodio::Sink::try_new(&handle).unwrap();
+
+    let file = std::fs::File::open("assets/sounds/Juicy.mp3").unwrap();
+    sink.append(rodio::Decoder::new(BufReader::new(file)).unwrap());
+
+    sink.sleep_until_end();
+
+    // // Open the MP3 file
+    // let file = File::open("path/to/yourfile.mp3").unwrap();
+    // let reader = BufReader::new(file);
+
+    // // Decode the MP3 file
+    // let decoder = Decoder::new(reader).unwrap();
+
+    // // You can now analyze the audio data, e.g., perform a Fourier Transform for spectrum analysis
+    // // ...
+
+    // // Play the audio
+    // let device = rodio::default_output_device().unwrap();
+    // rodio::play_raw(&device, decoder.convert_samples());
 }
